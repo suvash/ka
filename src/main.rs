b1f34b5d74330209204cd5c7c576731804a7c8c2
Suvash Thapaliya <suvash@gmail.com>
2021-05-29 00:38:50 +0200

    Basic editor display and cursor handling

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+#![warn(clippy::all, clippy::pedantic)]
+use ka::editor::Editor;
 
 fn main() {
-    let _raw_stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(k) => match k {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => {
-                    println!("Exiting");
-                    break;
-                }
-                _ => println!("{:?}\r", k),
-            },
-            Err(err) => die(err),
-        }
-    }
+    Editor::new().run();
 }
